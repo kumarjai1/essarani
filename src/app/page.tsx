cb442927a5e6e1fdd25ee03cd850cdcdfd700621
Jai Kumar <jaiessarani@gmail.com>
2024-12-27 11:44:45 -0500

    minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import Blog from "@/components/sections/blog";
 import ContactForm from "@/components/sections/contact-form";
-import CTA from "@/components/sections/cta";
 import FAQ from "@/components/sections/faq";
-import Features from "@/components/sections/features";
 import Footer from "@/components/sections/footer";
 import Header from "@/components/sections/header";
 import Hero from "@/components/sections/hero";
@@ -11,8 +8,6 @@ import Logos from "@/components/sections/logos";
 import Pricing from "@/components/sections/pricing";
 import Problem from "@/components/sections/problem";
 import Solution from "@/components/sections/solution";
-import Testimonials from "@/components/sections/testimonials";
-import TestimonialsCarousel from "@/components/sections/testimonials-carousel";
 
 export default function Home() {
   return (
@@ -29,6 +24,8 @@ export default function Home() {
       <Pricing />
       <FAQ />
       <ContactForm />
+      {/* <RegistrationForm /> */}
+
       {/* <Blog /> */}
       {/* <CTA /> */}
       <Footer />
--- src/components/sections/contact-form.tsx
+++ src/components/sections/contact-form.tsx
@@ -1,5 +1,21 @@
 "use client";
 
+// import { toast } from "sonner";
+// import { useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import * as z from "zod";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { Button } from "@/components/ui/button";
+
 export default function ContactForm() {
   return (
     <div
@@ -15,3 +31,104 @@ export default function ContactForm() {
     </div>
   );
 }
+
+// const formSchema = z.object({
+//   name: z.string().min(2),
+//   email: z.string(),
+//   phone: z.string(),
+//   address: z.string().optional(),
+// });
+
+// export function RegistrationForm() {
+//   const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//   });
+
+//   function onSubmit(values: z.infer<typeof formSchema>) {
+//     try {
+//       console.log(values);
+//       toast.info(
+//         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+//           <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+//         </pre>
+//       );
+//     } catch (error) {
+//       console.error("Form submission error", error);
+//       toast.error("Failed to submit the form. Please try again.");
+//     }
+//   }
+
+//   return (
+//     <Form {...form}>
+//       <form
+//         onSubmit={form.handleSubmit(onSubmit)}
+//         className="space-y-8 max-w-3xl mx-auto py-10"
+//       >
+//         <FormField
+//           control={form.control}
+//           name="name"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormLabel>Full Name</FormLabel>
+//               <FormControl>
+//                 <Input placeholder="John Doe" type="text" {...field} />
+//               </FormControl>
+//               <FormDescription>Enter your first and last name</FormDescription>
+//               <FormMessage />
+//             </FormItem>
+//           )}
+//         />
+
+//         <FormField
+//           control={form.control}
+//           name="email"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormLabel>Email</FormLabel>
+//               <FormControl>
+//                 <Input placeholder="john@doe.com" type="email" {...field} />
+//               </FormControl>
+//               <FormDescription>Enter your email address</FormDescription>
+//               <FormMessage />
+//             </FormItem>
+//           )}
+//         />
+
+//         <FormField
+//           control={form.control}
+//           name="phone"
+//           render={({ field }) => (
+//             <FormItem className="flex flex-col items-start">
+//               <FormLabel>Phone number</FormLabel>
+//               <FormControl className="w-full">
+//                 <Input type="tel" placeholder="234-567-8901" {...field} />
+//               </FormControl>
+//               <FormDescription>Enter your phone number.</FormDescription>
+//               <FormMessage />
+//             </FormItem>
+//           )}
+//         />
+
+//         <FormField
+//           control={form.control}
+//           name="address"
+//           render={({ field }) => (
+//             <FormItem>
+//               <FormLabel>Address</FormLabel>
+//               <FormControl>
+//                 <Input
+//                   placeholder="234 Street Ave. #1A, Charlotte, NC"
+//                   type="text"
+//                   {...field}
+//                 />
+//               </FormControl>
+//               <FormDescription>Enter your street address</FormDescription>
+//               <FormMessage />
+//             </FormItem>
+//           )}
+//         />
+//         <Button type="submit">Submit</Button>
+//       </form>
+//     </Form>
+//   );
+// }
